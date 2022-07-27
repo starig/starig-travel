5ec521c6968e413e45eab32a0f580d4b62b1bd26
srarig <starkovivan3@gmail.com>
2022-07-28 00:00:59 +0800

    CSS search input

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,11 @@ interface IHome {
 const Home:NextPage<IHome> = ({ places }) => {
   return (
     <Layout>
-        <SearchSection />
+        <div style={
+            {width: '80%', margin: '0 auto'}
+        }>
+            <SearchSection />
+        </div>
     </Layout>
   )
 }
