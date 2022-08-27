7826325841a61644a8651d24635de1791724e63f
srarig <starkovivan3@gmail.com>
2022-08-27 13:32:26 +0800

    Стилизация Inforamtion

---
+++ app/components/screens/place/Information/Information.tsx
@@ -0,0 +1,36 @@
+import React, {FC} from 'react';
+import {IPlace} from "../../../../types/place";
+import styles from './Information.module.scss';
+import {FaMapMarkerAlt, FaStar, FaCalendar} from 'react-icons/fa';
+
+
+const Information: FC<{place: IPlace}> = ({place}) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.heading}>
+                <FaMapMarkerAlt color={'#e8e8e8'} size={20}/>
+                <h1>{place.location.city + ', ' + place.location.country}</h1>
+            </div>
+
+            <p>
+                {place.description}
+            </p>
+
+            <div className={styles.additional}>
+
+                <div className={styles.rating}>
+                    <FaStar color={'#FDAE32'} size={18} />
+                    <span>{place.rating}/10</span>
+                </div>
+
+                <div className={styles.rating}>
+                    <FaCalendar color={'#565656'} size={18} />
+                    <span>{place.duration}</span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Information;
--- pages/api/places/index.ts
+++ pages/api/places/index.ts
@@ -4,7 +4,10 @@ import {NextApiRequest, NextApiResponse} from "next";
 export const places: IPlace[] = [
     {
         slug: 'kyoto',
-        description: 'Tokyo is a capital of great Japan!',
+        description: 'Invidunt dignissim orci dictumst eruditi possim quaeque vulputate purus alia duo ' +
+            'iaculis class phasellus eu nisl menandri saperet oporteat aperiri partiendo quaerendum elementum ' +
+            'ius dapibus ac ocurreret an nostrum eripuit nunc blandit maecenas donec feugiat persecuti tation ' +
+            'primis arcu simul nihil eius alienum qualisque justo in fusce sumo splendide ea',
         distance: 10324,
         location: {country: 'Japan', city: 'Kyoto'},
         imagePath: 'http://localhost:3000/images/places/japan.jpg',
@@ -15,7 +18,10 @@ export const places: IPlace[] = [
     },
     {
         slug: 'venezia',
-        description: 'Venezia is a capital of great Japan!',
+        description: 'Appareat nullam ne accumsan legere mutat eripuit ad legimus mazim sem vis explicari' +
+            ' tristique ipsum scripserit sit option accusata pulvinar pri vix justo mi himenaeos prodesset consectetur' +
+            ' mucius delicata libero quisque ea eirmod massa nisl idque fusce convenire percipit porta' +
+            ' lobortis nihil possim corrumpit viderer nunc nascetur tota quem scripserit',
         distance: 9454,
         location: {country: 'Italy', city: 'Venezia'},
         imagePath: 'http://localhost:3000/images/places/italy.jpg',
